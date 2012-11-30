b57f6519740380ba6b2016947d2f82f2522b5e25
Nat Pryce <sw@natpryce.com>
2012-11-30 09:48:47 +0000

    script to add the Quick2Wire software repository to apt on Raspbian

--- setup-apt.sh
+++ setup-apt.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-echo "test passed"
+# Ensure GPG is initialised for the current user.  If it is not, apt-key fails!
+gpg --list-keys > /dev/null
+
+# Install the Quick2Wire key for authenticating software packages
+curl http://dist.quick2wire.com/software@quick2wire.com.gpg.key | sudo apt-key add -
+
+sudo cat >> /etc/apt/sources.list  <<EOF
+
+# Quick2Wire Software
+deb http://dist.quick2wire.com/raspbian wheezy main 
+deb-src http://dist.quick2wire.com/raspbian wheezy main
+EOF
+
+sudo apt-get update
+
+echo "You can now install Quick2Wire software packages with the apt-get command"
+echo "For example: sudo apt-get install quick2wire-gpio-admin"
