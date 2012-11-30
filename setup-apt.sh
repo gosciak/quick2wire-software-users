c89ece99af575aaa0b3b50c7f54764a23c27752d
Nat Pryce <sw@natpryce.com>
2012-11-30 10:07:48 +0000

    fixing sudo problem

--- setup-apt.sh
+++ setup-apt.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 set -e
 
-# Ensure GPG is initialised for the current user.  If it is not, apt-key fails!
-gpg --list-keys > /dev/null
-
-sudo mkdir -p /etc/apt/sources.list.d/
-(sudo cat > /etc/apt/sources.list.d/quick2wire.list) <<EOF
+function listfile() {
+cat <<EOF
 # Quick2Wire Software
 deb http://dist.quick2wire.com/raspbian wheezy main 
 deb-src http://dist.quick2wire.com/raspbian wheezy main
 EOF
+}
+
+# Ensure GPG is initialised for the current user.  If it is not, apt-key fails!
+gpg --list-keys > /dev/null
 
 # Install the Quick2Wire key for authenticating software packages
 curl -s http://dist.quick2wire.com/software@quick2wire.com.gpg.key | sudo apt-key add -
 
+# Configure the package manager with the location of the Quick2Wire repository
+sudo mkdir -p /etc/apt/sources.list.d/
+listfile | sudo cat > /etc/apt/sources.list.d/quick2wire.list
 sudo apt-get update
 
 echo "You can now install Quick2Wire software packages with the apt-get command"
