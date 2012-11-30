c5d7004f88a97a39988518159dcc405e325b4211
Nat Pryce <sw@natpryce.com>
2012-11-30 10:15:54 +0000

    more privilege control

--- setup-apt.sh
+++ setup-apt.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 set -e
 
-function listfile() {
-cat <<EOF
-# Quick2Wire Software
-deb http://dist.quick2wire.com/raspbian wheezy main 
-deb-src http://dist.quick2wire.com/raspbian wheezy main
-EOF
-}
+# Note: this MUST be an HTTPS URL to ensure that the correct configuration is installed
+BASEURL=https://github.com/quick2wire/quick2wire-software-users/blob/master/
 
 # Ensure GPG is initialised for the current user.  If it is not, apt-key fails!
 gpg --list-keys > /dev/null
 
 # Install the Quick2Wire key for authenticating software packages
-curl -s http://dist.quick2wire.com/software@quick2wire.com.gpg.key | sudo apt-key add -
+curl -s $BASEURL/software@quick2wire.com.gpg.key | sudo apt-key add -
 
 # Configure the package manager with the location of the Quick2Wire repository
 sudo mkdir -p /etc/apt/sources.list.d/
-listfile | sudo cat > /etc/apt/sources.list.d/quick2wire.list
+curl -s $BASEURL/quick2wire.list | sudo cat > /etc/apt/sources.list.d/quick2wire.list
+
+# Refresh the list of available packages
 sudo apt-get update
 
 echo "You can now install Quick2Wire software packages with the apt-get command"
