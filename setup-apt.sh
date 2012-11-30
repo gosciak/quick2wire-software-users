b242ec2d82e889ecdbb465dcfc65ff77ad05a27d
Nat Pryce <sw@natpryce.com>
2012-11-30 10:22:05 +0000

    more sudo wrangling

--- setup-apt.sh
+++ setup-apt.sh
@@ -8,11 +8,11 @@ BASEURL=https://raw.github.com/quick2wire/quick2wire-software-users/master
 gpg --list-keys > /dev/null
 
 # Install the Quick2Wire key for authenticating software packages
-curl -s $BASEURL/software@quick2wire.com.gpg.key | sudo apt-key add -
+curl -s $BASEURL/software@quick2wire.com.gpg.key | (sudo apt-key add -)
 
 # Configure the package manager with the location of the Quick2Wire repository
 sudo mkdir -p /etc/apt/sources.list.d/
-curl -s $BASEURL/quick2wire.list | sudo cat > /etc/apt/sources.list.d/quick2wire.list
+curl -s $BASEURL/quick2wire.list | (sudo cat > /etc/apt/sources.list.d/quick2wire.list)
 
 # Refresh the list of available packages
 sudo apt-get update
